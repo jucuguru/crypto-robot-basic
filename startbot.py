28b6ee358d97342f0b8b28db83d100ef6e8d5ec3
jucuguru <jucuguru@gmail.com>
2018-12-07 22:46:51 +0800

    Add files via upload

---
+++ startbot.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+# ##################################################################
+#  Quant Robot for Cryptocurrency
+# author: Huang kejie
+# Date : 2018.11.26
+##################################################################
+
+# import bulit in package
+#import argparse
+import sys
+
+from  qbrobot.quantrobot import QuantRobot
+
+if __name__ == '__main__':
+
+    robot = QuantRobot(  )
+    robot.setDaemon(True)
+
+    try :
+        robot.start()
+        robot.join()
+
+        print('MainThread runner normal shutdown.')
+        sys.exit(0)
+
+    except (KeyboardInterrupt, SystemExit):
+        print('MainThread runner shutdown.')
+        robot.exit()
+        robot.join()
+        sys.exit(1)
+
+
+
+
+
+
